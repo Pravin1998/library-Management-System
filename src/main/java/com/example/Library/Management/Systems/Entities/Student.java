a4bfc93a3203851cbf3c9fce91286ef1957d9947
Pravin Prajapati <pp135147@gmail.com>
2023-11-05 03:56:29 +0530

    1

---
+++ src/main/java/com/example/Library/Management/Systems/Entities/Student.java
@@ -0,0 +1,38 @@
+package com.example.Library.Management.Systems.Entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Entity //This means it's a structure that will be reflected in Database
+@Table(name = "student") //This class will have a table whose name is studentTable
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+
+    @Id
+    private Integer studentId; //This will behave as the Primary Key
+
+    private String name;
+
+    private int age;
+
+    @Column(name = "contactNo",unique = true,nullable = false)
+    private String mobNo;
+
+    private String emailId;
+
+    private String bloodGroup;
+
+    @OneToOne(mappedBy = "student",cascade = CascadeType.ALL)
+    private LibraryCard libraryCard;
+
+
+    //This should also have libraryCard Information
+
+}
